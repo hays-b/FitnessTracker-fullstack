a8682697668ef41bf42a52ccdf98bbcf8b6d9239
Hays <haysbondy@icloud.com>
2022-06-08 23:29:14 -0500

    adding meta tags for link sharing

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,11 +1,11 @@
 import React from "react";
-import happiness from '../assets/happiness.png'
+import happiness from "../assets/happiness.png";
 
 const Home = () => {
   return (
     <div className="routine-col">
       <h1>Welcome to Fitness Tracker!</h1>
-      <img className='img' src={happiness} alt="Happiness" width='500px'/>
+        <img className="img" src={happiness} alt="Happiness" />
     </div>
   );
 };
