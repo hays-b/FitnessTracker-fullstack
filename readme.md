19dc0062b47d8f79bdd444c2a6df99d508f4b5c6
Hays <haysbondy@icloud.com>
2022-06-10 18:22:09 -0500

    added to home page and readme

---
+++ readme.md
@@ -0,0 +1,18 @@
+This repo is a template for a workout sharing site where users can post their own routines for the community to see. It can be viewed [here](https://fitness-tracker-react-app.herokuapp.com/).
+
+## Tech stack
+
+- React.js
+- Express.js
+- Node.js
+- PostgreSQL
+- Heroku
+- React-Toastify
+- Bcrypt
+- JSON Web Token
+
+## Collaborators
+
+- Hays Bondy [github](https://github.com/hays-b)
+- Jaden Short (back-end) [github](https://github.com/Biospear)
+- Chudi Onubogu (front-end) [github](https://github.com/chudiisnumber1)
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,11 +1,30 @@
 import React from "react";
-import happiness from "../assets/happiness.png";
+import { Link } from "react-router-dom"
+import bicep from '../assets/fitness-logo.png'
+import runner from '../assets/running-heart.png'
+import useAuth from "../hooks/useAuth";
 
 const Home = () => {
+  const { user } = useAuth();
+
   return (
     <div className="routine-col">
-      <h1>Welcome to Fitness Tracker!</h1>
-        <img className="img" src={happiness} alt="Happiness" />
+      <h1>Welcome to Fitness Tracker{user.username ? `, ${user.username}`: null }!</h1>
+      <p className='center'>Your one stop shop for fitness inspiration and community collaboration!</p>
+      <div className='home-row'>
+        <img className="home-img" src={runner} alt="Runner" />
+      <Link to='/routines' className='home-card'>
+        <p>Browse workout routines and find one that works for you! Or sign in to create your own to share with others!</p>
+        <h3 className='arrow'>{'>'}</h3>
+      </Link>
+      </div>
+      <div className='home-row'>
+      <Link to='/activities' className='home-card'>
+        <h3 className='arrow'>{'<'}</h3>
+        <p>Browse individual activities to gain inspiration or to add to your own routines!</p>
+      </Link>
+        <img className="home-img" src={bicep} alt="Bicep" />
+        </div>
     </div>
   );
 };
